7cac84592d5ae7b809db1496c4429c66ba7e8551
khanderaopragati <pragatikrao97@gmail.com>
2023-11-24 15:55:57 +0530

    intro to js and variables

---
+++ test/variables.js
@@ -0,0 +1,16 @@
+// variables are containers to store Data.
+// java script variables can be declaired in four ways. 
+
+// 1. automatically( var default)
+// 2. using var 
+// 3. using let 
+// 4. using const
+
+// let/var/const= value 
+
+const marks = 50;
+console.log(marks);
+name= "pragati";
+console.log(name);
+sirname = "khanderao"
+console.log(sirname);
